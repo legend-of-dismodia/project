8fe1e8ac4552281d535531ccbfb7fa0369415a12
Clyde <clydeponteville@hotmail.fr>
2018-12-11 10:35:20 +0100

    base de donnee
    creation base de donnee

---
+++ src/Entity/Save.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SaveRepository")
+ */
+class Save
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $level;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $life;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $mana;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $xp;
+
+    /**
+     * @ORM\Column(type="time")
+     */
+    private $playtime;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="saves")
+     */
+    private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Inventory", mappedBy="save")
+     */
+    private $inventories;
+
+    public function __construct()
+    {
+        $this->inventories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getLevel(): ?int
+    {
+        return $this->level;
+    }
+
+    public function setLevel(int $level): self
+    {
+        $this->level = $level;
+
+        return $this;
+    }
+
+    public function getLife(): ?int
+    {
+        return $this->life;
+    }
+
+    public function setLife(int $life): self
+    {
+        $this->life = $life;
+
+        return $this;
+    }
+
+    public function getMana(): ?int
+    {
+        return $this->mana;
+    }
+
+    public function setMana(int $mana): self
+    {
+        $this->mana = $mana;
+
+        return $this;
+    }
+
+    public function getXp(): ?int
+    {
+        return $this->xp;
+    }
+
+    public function setXp(int $xp): self
+    {
+        $this->xp = $xp;
+
+        return $this;
+    }
+
+    public function getPlaytime(): ?\DateTimeInterface
+    {
+        return $this->playtime;
+    }
+
+    public function setPlaytime(\DateTimeInterface $playtime): self
+    {
+        $this->playtime = $playtime;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Inventory[]
+     */
+    public function getInventories(): Collection
+    {
+        return $this->inventories;
+    }
+
+    public function addInventory(Inventory $inventory): self
+    {
+        if (!$this->inventories->contains($inventory)) {
+            $this->inventories[] = $inventory;
+            $inventory->setSave($this);
+        }
+
+        return $this;
+    }
+
+    public function removeInventory(Inventory $inventory): self
+    {
+        if ($this->inventories->contains($inventory)) {
+            $this->inventories->removeElement($inventory);
+            // set the owning side to null (unless already changed)
+            if ($inventory->getSave() === $this) {
+                $inventory->setSave(null);
+            }
+        }
+
+        return $this;
+    }
+}
