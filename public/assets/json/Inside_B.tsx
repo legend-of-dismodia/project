ad27dd62a13accd8fcd44fea499e9647cdc86306
Unknown <juliehenriet@gmail.com>
2018-12-14 12:11:51 +0100

    add phaser

---
+++ public/assets/json/Inside_B.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Inside_B" tilewidth="48" tileheight="48" tilecount="256" columns="16">
+ <image source="../tilesets/Inside_B.png" width="768" height="768"/>
+</tileset>
