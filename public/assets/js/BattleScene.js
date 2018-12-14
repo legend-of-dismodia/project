5f0c2b723129843a4fb525e9da06813b6cb95544
Unknown <obert.vincent@laposte.net>
2018-12-14 16:34:32 +0100

    ajout troll

--- public/assets/js/BattleScene.js
+++ public/assets/js/BattleScene.js
@@ -10,7 +10,7 @@ var BattleScene = new Phaser.Class({
     {
         // load resources
     this.load.spritesheet("player", "../assets/spritesheet/princessfinal clone.png", { frameWidth: 80, frameHeight: 80 });
-    this.load.spritesheet("souris", "../assets/spritesheet/troll.png", { frameWidth: 150, frameHeight: 200});
+    this.load.spritesheet("souris", "../assets/spritesheet/troll2.png", { frameWidth: 218, frameHeight: 300});
     this.load.image("fond", "../assets/spritesheet/battle.png");
     },
     create: function ()
